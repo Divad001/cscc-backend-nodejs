a3ee3207782aa13cb2c85fe5797513226d869f0b
Divad001 <gyoredavid1993@gmail.com>
2020-10-16 13:09:22 +0200

    feat: setup express server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
+import express from 'express';
+
 export const startApp = async (): Promise<void> => {
   // create application
-  // start application
+  const app = express();
+
+  app.get('/', (_req, res) => {
+    res.send('Hello World')
+  });
+  // start application.
+  app.listen(8080, () => console.log('Server running'))
 };
