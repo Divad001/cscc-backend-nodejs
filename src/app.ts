127aad93b9b23ee2384e2c31e115a46af4cd85c7
Divad001 <gyoredavid1993@gmail.com>
2020-10-17 18:03:51 +0200

    feat: add /breweries endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,36 @@
 import express from 'express';
+import morgan from 'morgan';
+import axios from 'axios';
 
 export const startApp = async (): Promise<void> => {
   // create application
   const app = express();
-  // create midleware log
-  const morgan = require('morgan');
+  // add midleware log
   app.use(morgan('tiny'));
   app.get('/', (_req, res) => {
-    res.send('Hello World')
+    res.send('Hello World');
   });
+  app.get('/breweries', async (_req, res) => {
+    try {
+      const result = await axios.get('https://api.openbrewerydb.org/breweries');
+      res.send(result.data);
+    } catch (error) {
+      res.send(error.response.status);
+    }
+  });
+  /* app.get('/test', async (_req, res) => {
+    const result = await getBreweries();
+    res.send(result);
+  }); */
+  //// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  /* async function getBreweries() {
+    try {
+      const result = await axios.get('https://api.openbrewerydb.org/breweries');
+      return result.data;
+    } catch (error) {
+      console.log(error);
+    }
+  } */
   // start application.
-  app.listen(8080, () => console.log('Server running'))
+  app.listen(8080, () => console.log('Server running'));
 };
