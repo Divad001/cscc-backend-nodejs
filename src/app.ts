10d404af45c756b36ece08579ac777c410992548
Divad001 <gyoredavid1993@gmail.com>
2020-10-17 17:04:15 +0200

    feat: add log

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express from 'express';
 export const startApp = async (): Promise<void> => {
   // create application
   const app = express();
-
+  // create midleware log
+  const morgan = require('morgan');
+  app.use(morgan('tiny'));
   app.get('/', (_req, res) => {
     res.send('Hello World')
   });
